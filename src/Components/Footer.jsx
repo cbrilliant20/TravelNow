65edfc6f8b6633009d5f6444d27311d48c39667f
Cameron Brilliant <cbrilliant20@gmail.com>
2021-08-20 16:37:25 -0400

    footer fixed on mobile

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,14 +4,13 @@ const Footer = () => {
   return (
     <section>
       <div class="bg-lightGray text-darkBlue mt-12 lg:mt-28 px-16 ">
-        <div class=" container flex  justify-between py-8 gap-8 lg:items-end items-start flex-wrap">
-          <div class="flex flex-col  w-1/4 justify-center">
-            <h3 class="text-orange text-xl font-semibold">
+        <div class=" container grid-cols-2 grid lg:flex justify-between py-8 gap-8  items-start flex-wrap ">
+          <div class="flex flex-col  w-4/5 lg:w-1/5  text-left">
+            <h3 class="text-orange text-xl font-semibold self-start">
               Travel<span class="text-darkBlue ">Now</span>
             </h3>
-            <p class="font-extralight hidden lg:block ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-              suscipit unde saepe maiores nulla laborum.
+            <p class="font-extralight  lg:block ">
+              Lorem ipsum sit amet consect adipi si cing elit.
             </p>
           </div>
 
@@ -27,23 +26,22 @@ const Footer = () => {
 
           <div class="flex flex-col   ">
             {" "}
-            <p class="text-xl font-bold">Tour</p>
+            <p class="text-xl font-bold">Support</p>
             <ul class="font-extralight">
-              <li>Thailand</li>
-              <li>Canada</li>
-              <li>Korea</li>
-              <li>Italy</li>
+              <li>Account</li>
+              <li>Legal</li>
+              <li>Contact</li>
+              <li>Privacy Policy</li>
             </ul>
           </div>
 
-          <div class="flex flex-col ">
+          <div class="flex flex-col items-start ">
             {" "}
-            <p class="text-xl font-bold">Tour</p>
+            <p class="text-xl font-bold">Useful Pages</p>
             <ul class="font-extralight">
-              <li>Thailand</li>
-              <li>Canada</li>
-              <li>Korea</li>
-              <li>Italy</li>
+              <li>Deals</li>
+              <li>FAQs</li>
+              <li>Why Us</li>
             </ul>
           </div>
         </div>
